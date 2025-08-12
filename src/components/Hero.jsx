8d27f0e4ba060e10f7855a62fe60d79598efc922
ABISHEK ANTONY <abishek8673@gmail.com>
2025-08-12 15:35:56 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ export default function Hero() {
   }
 
   const handleDownloadCV = () => {
-    window.open('https://drive.google.com/file/d/138UYyH6udXwcMoI6VSxHQ5JA2gXA03mM/view?usp=sharing', '_blank')
+    window.open('https://drive.google.com/file/d/1fr9HmXqwu2VG28DC7qzPbEmIaQOwZSg3/view?usp=sharing', '_blank')
   }
 
   return (
