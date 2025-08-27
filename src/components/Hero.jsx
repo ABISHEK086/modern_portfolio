b6a231dc81142f55877edf123b961e2b350bfc4a
ABISHEK ANTONY <abishek8673@gmail.com>
2025-08-27 16:53:11 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
 
   const handleDownloadCV = () => {
     window.open(
-      "https://student-performance-prediction-ml-enjugumqrmvssvnarrquav.streamlit.app/",
+      "https://drive.google.com/file/d/1rIraYSi9U49gAK0e856NrWDskAUEHFVR/view?usp=sharing/",
       "_blank"
     )
   }
