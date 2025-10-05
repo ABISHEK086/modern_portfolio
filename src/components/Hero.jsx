82a233d573abc56302395a2841d316c791f70c55
Abishek Antony <abishek8673@gmail.com>
2025-10-05 22:49:36 +0530

    updated successfully

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
 
   const handleDownloadCV = () => {
     window.open(
-      "https://drive.google.com/file/d/1y3UsBm9_DllGoSQTqYJWePx_faVIfzoK/view?usp=sharing/",
+      "https://drive.google.com/file/d/1UB3lJ7ECSfIFkE50VpGn6yNjSnezUato/view?usp=sharing",
       "_blank"
     )
   }
