2e62d1c15217b5d00de32ed2f6b75bcab884403c
ABISHEK ANTONY <abishek8673@gmail.com>
2025-11-03 16:42:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';       
 import Footer from './components/Footer'; 
 import Experience from './components/Experience';
-import Education from './components/Education';
+import Education from './components/Education'; 
 import './App.css';
 
 export default function App() {
