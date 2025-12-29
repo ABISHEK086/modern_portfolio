4ff22d87f02a86495551531b8d5dba970c18c75f
ABISHEK ANTONY <abishek8673@gmail.com>
2025-12-29 17:28:26 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
- import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { FiBriefcase, FiCalendar } from 'react-icons/fi';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
